d426185d026745a7738e147feedd22e963b87773
Lucas <xz351@hotmail.fr>
2019-11-29 10:17:14 +0100

    Declare all function

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -6,6 +6,9 @@ function type_check_v1(value, type) {
 console.log(type_check_v1(1, 'number'))
 console.log(type_check_v1(['amin'], 'array'))
 
-// function type_check_v2(value, conf) {
+function type_check_v2(value, conf) {
 
-// }
+}
+function type_check_v3(value, conf) {
+
+}
