62a7aadab46c27e0fd69f1662c9b345c6aefa344
Lucas <xz351@hotmail.fr>
2019-11-29 10:35:21 +0100

    Fix the type_check_v1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,6 +1,14 @@
 function type_check_v1(value, type) {
-  if (!value || !type) return false
-  return type == 'array' ? Array.isArray(value) : typeof value === type
+  switch (type) {
+    case 'array':
+      return Array.isArray(value);
+    case 'null':
+      return !value;
+    case 'undefined':
+      return value === undefined;
+    default:
+      return typeof value === type;
+  }
 }
 
 console.log(type_check_v1(1, 'number'))
@@ -9,6 +17,7 @@ console.log(type_check_v1(['amin'], 'array'))
 function type_check_v2(value, conf) {
 
 }
+
 function type_check_v3(value, conf) {
 
 }
