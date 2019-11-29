9e9cbbaae32a530189f08e3534eca79caaa23195
Lucas <xz351@hotmail.fr>
2019-11-29 10:45:50 +0100

    Fix the first check again

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,4 +1,5 @@
 function type_check_v1(value, type) {
+  type = type.toLowerCase();
   switch (type) {
     case 'array':
       return Array.isArray(value);
@@ -6,12 +7,15 @@ function type_check_v1(value, type) {
       return !value;
     case 'undefined':
       return value === undefined;
+    case 'object':
+      if (!value || Array.isArray(value)) return false;
+      return typeof value === type;
     default:
       return typeof value === type;
   }
 }
 
-console.log(type_check_v1(1, 'number'))
+console.log(type_check_v1({test: 'test'}, 'object'))
 console.log(type_check_v1(['amin'], 'array'))
 
 function type_check_v2(value, conf) {
