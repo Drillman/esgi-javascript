280454c9e210a5a93ccfb2cee957097e85707ca8
Lucas <xz351@hotmail.fr>
2019-11-29 10:15:42 +0100

    Inplement first type check function

---
+++ exercice-2/type-check.js
@@ -0,0 +1,11 @@
+function type_check_v1(value, type) {
+  if (!value || !type) return false
+  return type == 'array' ? Array.isArray(value) : typeof value === type
+}
+
+console.log(type_check_v1(1, 'number'))
+console.log(type_check_v1(['amin'], 'array'))
+
+// function type_check_v2(value, conf) {
+
+// }
